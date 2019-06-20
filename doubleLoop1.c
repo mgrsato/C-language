ae970a256540e2bbd9fc14261b45cfdb96342476
mgrsato <mgrsato@gmail.com>
2019-06-20 10:56:12 +0900

    Create doubleLoop1.c

---
+++ doubleLoop1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    int j;
+    for (i = 0; i < 4; i++){
+        for (j = 0; j < 4; j++){
+            printf("■");
+        }
+        printf("\n");
+    }
+    return 0;
+}
